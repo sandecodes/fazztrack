444034c7cdfde854e3204c418b92de161adf6738
sandecodes <muhammadsandy74@gmail.com>
2021-12-03 10:35:50 +0700

    fitur insert character khusus

--- tambah-proses.php
+++ tambah-proses.php
@@ -3,10 +3,10 @@
 require 'connection/connection.php';
 
 if(isset($_POST['simpan'])) {
-    $nama_produk = $_POST['nama_produk'];
-    $keterangan = $_POST['keterangan'];
-    $harga = $_POST['harga'];
-    $jumlah = $_POST['jumlah'];
+    $nama_produk = addslashes($_POST['nama_produk']);
+    $keterangan = addslashes($_POST['keterangan']);
+    $harga = addslashes($_POST['harga']);
+    $jumlah = addslashes($_POST['jumlah']);
 
     $simpan = mysqli_query($conn, "INSERT INTO produk VALUES ('','$nama_produk','$keterangan','$harga','$jumlah')");
 
